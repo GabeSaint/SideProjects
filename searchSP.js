149dfab7f8f0532f74db554de97f4f5723c52b50
GabeSaint <38449112+GabeSaint@users.noreply.github.com>
2019-10-10 16:53:39 -0700

    Update searchSP.js

--- searchSP.js
+++ searchSP.js
@@ -1,17 +1,30 @@
-function searchSP() { 
-    var patt = /\<(.*?)\>/i; // Regex to remove all but the list item
-    let input = document.getElementById('searchbar').value 
-    input=input.toLowerCase(); 
-    let x = document.getElementsByClassName('searchableItem'); 
-    x = x.replace(patt,'');
+function searchSP()
+{
 
-      
-    for (i = 0; i < x.length; i++) {  
-        if (!x[i].innerHTML.toLowerCase().includes(input)) { 
-            x[i].style.display="none"; 
-        } 
-        else { 
-            x[i].style.display="list-item";                  
-        } 
-    } 
-} 
+
+    var patt = /\<(.*?)\>/i; // Regex to remove all but the list item
+
+
+    var input = document.getElementById('searchbar').value
+    input=input.toLowerCase();
+
+
+    let x = document.getElementsByClassName('searchableItem');
+
+    for (i = 0; i < x.length; i++)
+    {
+
+
+
+        if (!x[i].innerHTML.toLowerCase().replace(patt,'').includes(input))
+        {
+            x[i].style.display="none";
+        }
+        else
+        {
+            x[i].style.display="list-item";
+        }
+
+
+    }
+}
