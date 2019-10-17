045a2d34b1f3c17f567ef4145ddaac928c9759c9
GabeSaint <38449112+GabeSaint@users.noreply.github.com>
2019-10-17 14:36:43 -0700

    Added in cleaner appearance

--- searchSP.js
+++ searchSP.js
@@ -1,4 +1,4 @@
-function searchSP()
+  function searchSP()
 {
 
 
@@ -8,12 +8,38 @@ function searchSP()
     var input = document.getElementById('searchbar').value
     input=input.toLowerCase();
 
+    if (input != "")
+    {
+
+      var header6 = document.getElementsByClassName('departmentHeader');
+      var backButton = document.getElementsByClassName('back');
+      document.getElementById('letters').style.display = 'none';
+
+      for (var i = 0; i < header6.length; i++)
+      {
+      header6[i].style.display = 'none';
+      backButton[i].style.display = 'none';
+      }
+
+    }
+    else{
+      var header6 = document.getElementsByClassName('departmentHeader');
+      var backButton = document.getElementsByClassName('back');
+      document.getElementById('letters').style.display = 'block';
+
+      for (var i = 0; i < header6.length; i++)
+      {
+      header6[i].style.display = 'block';
+      backButton[i].style.display = 'block';
+      }
+
+    }
+
 
     let x = document.getElementsByClassName('searchableItem');
 
     for (i = 0; i < x.length; i++)
     {
-
 
 
         if (!x[i].innerHTML.toLowerCase().replace(patt,'').includes(input))
@@ -22,9 +48,23 @@ function searchSP()
         }
         else
         {
+            var j = 0;
+
             x[i].style.display="list-item";
+            j = j+1;
+
         }
 
 
     }
+
+    if (j == undefined)
+    {
+      document.getElementById("errorMess").innerHTML = "We couldn't find any forms with those key words, try another keyphrase";
+    }
+    else
+    {
+      document.getElementById("errorMess").innerHTML = "";
+    }
+
 }
