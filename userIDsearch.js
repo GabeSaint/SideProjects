ad50ad2e56acb327c581fe9b880614a954508f55
GabeSaint <38449112+GabeSaint@users.noreply.github.com>
2019-10-23 13:07:15 -0700

    Update userIDsearch.js

--- userIDsearch.js
+++ userIDsearch.js
@@ -4,7 +4,7 @@ function userIDURL()
 var userID = document.getElementById("textField").value;
 
 var url = "https://training.wp.nau.edu/"
-var locationURL = url+userID;
+var locationURL = url+userID+"-sandbox";
 
 location.replace(locationURL);
 
