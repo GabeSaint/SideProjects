029fa6543b57749d6ac440fa6296a97956e55c17
Gabriel J Santos <gjs88@iris.nau.edu>
2020-02-03 12:01:02 -0700

    Update functionalityIN.js

--- IN/functionalityIN.js
+++ IN/functionalityIN.js
@@ -1,45 +1,32 @@
 // Get the modal
-var modal = document.getElementById("myModal");
-var modal2 = document.getElementById("myModal2");
+var modal = document.getElementsByClassName('modal');
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-var btn2 = document.getElementById("myBtn2");
+var btn = document.getElementsByClassName("myBtn");
+
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("close");
 
 // When the user clicks the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+btn[0].onclick = function() {
+    modal[0].style.display = "block";
 }
 
+btn[1].onclick = function() {
+    modal[1].style.display = "block";
+}
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+span[0].onclick = function() {
+    modal[0].style.display = "none";
 }
 
+span[1].onclick = function() {
+    modal[1].style.display = "none";
+}
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-
-
-btn2.onclick = function() {
-  modal2.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal2.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal2) {
-    modal2.style.display = "none";
-  }
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
