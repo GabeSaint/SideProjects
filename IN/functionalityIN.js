71837989014861dc253186c7454ac530d1cd23c1
Gabriel J Santos <gjs88@iris.nau.edu>
2020-02-21 11:25:53 -0700

    Updates

--- IN/functionalityIN.js
+++ IN/functionalityIN.js
@@ -52,27 +52,3 @@
                 modal.style.display = "none";
             }
         };
-
-//XXXXXXXXXXXXXXXXXXXXXXX    Modified old code    XXXXXXXXXXXXXXXXXXXXXXXXXX
-
-// Get the modal
-
-        var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-        var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-        var span = modal.getElementsByClassName("close")[0]; // Modified by dsones uk
-
-// When the user clicks on the button, open the modal
-
-        btn.onclick = function() {
-
-            modal.style.display = "block";
-        }
-
-// When the user clicks on <span> (x), close the modal
-        span.onclick = function() {
-            modal.style.display = "none";
-        }
